8bbebae80c9a9876d7b8bf56b460da2aadddd515
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-03-20 19:37:17 +0530

    saving state

---
+++ common.py
@@ -0,0 +1,2 @@
+def generateKey():
+    print("need to implement function to generate key pair")
--- slave.py
+++ slave.py
@@ -1,2 +1,12 @@
 import socket
 import ssl
+import subprocess
+from common import *
+
+try:
+    privatekey = open("privatekey.pem", 'r').read()
+    publickey = open("publickey.pem", 'r').read()
+    print(privatekey)
+    print(publickey
+except FileNotFoundError:
+    generateKey()
