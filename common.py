c55280bf3a74a757103e10c68e11446b11fc35f9
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-04-22 16:54:13 +0530

    Implement loading and saving of parameters

--- common.py
+++ common.py
@@ -1,23 +1,46 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
 import os
+import pickle
 
 class Common:
-    def __init__(self):
-        self.privateKey = None
-        self.loadParam()
-    def generatekey(param):
-        self.privateKey = ec.generate_private_key(
-            ec.SECP256K1(), default_backend()
-        )
-    def saveParams():
-        pass
-    def signMessage(self, data):
-        signature = self.privateKey.sign(
-            data,
-            ec.ECDSA(hashes.SHA512()) #check if it's secure
-        )
-        return signature
-    def loadParam(self, filename = None):
-        pass
+	def __init__(self):
+		try:
+			self.loadParam()
+		except FileNotFoundError:
+			self.param = {}
+			self.privateKey = ec.generate_private_key(
+				ec.SECP256K1(),
+				default_backend()
+			)
+			self.param['friendlyName'] = input("Enter your friendly name: ")
+			self.saveParams()
+
+	def saveParams(self):
+		serializedPrivateKey = self.privateKey.private_bytes(
+			encoding = serialization.Encoding.PEM,
+			format = serialization.PrivateFormat.PKCS8,
+			encryption_algorithm = serialization.NoEncryption()
+		)
+		data  = (self.param, serializedPrivateKey)
+		with open('param.pickle', 'wb') as f:
+			pickle.dump(data, f)
+
+	def signMessage(self, data):
+		signature = self.privateKey.sign(data,ec.ECDSA(hashes.SHA512())) #check if it's secure
+		return signature
+
+	def loadParam(self):
+		with open('param.pickle', 'rb') as f:
+			data = pickle.load(f)
+			self.param = data[0]
+			self.privateKey = serialization.load_pem_private_key(
+				data[1],
+				password = None,
+				backend = default_backend()
+			)
+
+if __name__ == '__main__':
+	c = Common()
