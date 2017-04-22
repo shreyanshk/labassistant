d4ee92344b1eb16b577855eb4997dd8ab9f34a8d
Shreyansh Khajanchi <shreyansh_k@live.com>
2017-04-23 00:44:11 +0530

    Implement heartbeat to allow slaves to find

--- master.py
+++ master.py
@@ -2,6 +2,7 @@ import socket
 from common import Common
 from time import sleep
 import pickle
+import threading
 
 class Master(Common):
 	def shutdown(self, timer = 0, force = False):
@@ -17,12 +18,22 @@ class Master(Common):
 		}
 		data = self.signedCmd(cmd)
 		self.mcastSocket.sendto(data, self.mcastGroup)
-		
+
 	def run(self, mcastGroup = None):
 		if mcastGroup == None:
 			self.mcastGroup = ('224.3.29.71', 24979)
 		self.mcastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 		self.mcastSocket.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+		heartbeatThread = threading.Thread(target = self.heartbeat)
+		heartbeatThread.daemon = False
+		heartbeatThread.start()
+
+	def heartbeat(self, timer = 5):
+		data = {
+			'serializedPublicKey': self.param['serializedPublicKey'],
+			'friendlyName': self.param['friendlyName']
+		}
+		data = pickle.dumps(data)
 		while (True):
-			self.displayString("Hello")
-			sleep(5)
+			self.mcastSocket.sendto(data, self.mcastGroup)
+			sleep(timer)
