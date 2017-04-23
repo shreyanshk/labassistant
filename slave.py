4d7a82de25b6365cef1b1e8b7a9285d3fb733eaf
Abhishek <abhishek.joshi32@gmail.com>
2017-04-23 18:05:53 +0530

    simplified verifyMsg function's def and implemented listener in slave

--- slave.py
+++ slave.py
@@ -8,11 +8,8 @@ from cryptography.hazmat.primitives import hashes
 from common import Common
 import os
 from time import sleep
+
 class Slave(Common):
-	'''def shutdown(force,timer):
-		print("The system will shut down in" + timer + "seconds" )
-		sleep(timer)
-		os.system("shutdown now -h")'''
 	def run(self, mcastGroup = None):
 		if mcastGroup == None:
 			self.mcastGroup = ('224.3.29.71', 24979)
@@ -23,22 +20,26 @@ class Slave(Common):
 		self.mcastSocket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 		try:
 			masterSerializedPublicKey=self.param['masterSerializedPublicKey']
-			##Listen for commands
+			self.listener()
 		except KeyError:
 			print('Looking for master server now.')
-			rcvdata,masterAddress = self.mcastSocket.recvfrom(1024)
-			rcvdict = pickle.loads(rcvdata)
-			permission = input("Do you want to connect to: "
-				+ rcvdict['friendlyName']
-				+ ' at IP '+ str(masterAddress)
-				+ ' whose signature is \n\n'+rcvdict['serializedPublicKey'].decode()
-				+ '\nReply with [y/n]: '
-			)
-			if (permission == 'y') or (permission == 'Y'):
-				self.masterAuthenticate(rcvdict,masterAddress)
-			else:
-				print('Incomplete configuration, please rerun the program.')
-				exit()
+			heartbeat = False
+			while(~heartbeat):
+				rcvdata,masterAddress = self.mcastSocket.recvfrom(1024)
+				rcvdict = pickle.loads(rcvdata)
+				if (rcvdict['type'] == 'heartbeat'):
+					permission = input("Do you want to connect to: "
+						+ rcvdict['friendlyName']
+						+ ' at IP '+ str(masterAddress)
+						+ ' whose signature is \n\n'+rcvdict['serializedPublicKey'].decode()
+						+ '\nReply with [y/n]: '
+					)
+					if (permission == 'y') or (permission == 'Y'):
+						self.masterAuthenticate(rcvdict,masterAddress)
+						self.listener()
+					else:
+						print('Incomplete configuration, please rerun the program.')
+						exit()
 
 	def masterAuthenticate(self,masterData,masterAddress):
 		self.masterUcastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -61,8 +62,47 @@ class Slave(Common):
 		self.saveParams()
 		print(self.param)
 
-	def listener():
-		rcvdata,masterAddress = self.mcastSocket.recvfrom(1024)
-		rcvdict = pickle.loads(rcvdata)
-		print(rcvdict)
+	def listener(self):
+		print("Listening for commands from " + self.param['masterFriendlyName'])
+		masterSerializedPublicKey = self.param['masterSerializedPublicKey']
+		masterPublicKey = serialization.load_pem_public_key(
+				masterSerializedPublicKey,
+				backend = default_backend()
+			)
+		while True:
+			rcvdata,masterAddress = self.mcastSocket.recvfrom(1024)
+			rcvdict = pickle.loads(rcvdata)
+			if (rcvdict['type'] == 'cmd') and (self.verifyMsg(masterPublicKey, rcvdict)):
+				cmds = pickle.loads(rcvdict['data'])
+				function = self.dispatcher(cmds['cmd'])
+				if (function == None):
+					self.cmdNotFoundError(cmds['cmd'])
+				else:
+					function(cmds[cmds['cmd']])
+
+	def cmdNotFoundError(self, string):
+		data = {
+			'serializedPublicKey': self.param['serializedPublicKey'],
+			'friendlyName': self.param['friendlyName'],
+			'msg': 'cmdNotFoundError'
+		}
+
+# function definitions starts here
+
+	def displayString(self,string):
+		print(string)
+
+	def shutdown(self, force = False, timer = 0) :
+		print("The system will shut down in" + timer + "seconds" )
+		sleep(timer)
+		os.system("shutdown now -h")
+
+# function definitions end here
+
+	def dispatcher(self, string):
+		cmds = {
+			'displayString': self.displayString,
+			'shutdown': self.shutdown
+		}
+		return cmds[string]
 
